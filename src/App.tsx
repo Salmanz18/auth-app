f1912461c88eb4d2af480ec4b3879a5ce33a2332
Salmanz18 <18salmanz@gmail.com>
2023-11-25 20:46:06 +0530

    add loading and error state

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading, error } = useAuth0();
+
   return (
     <div className="column">
       <h1>Auth0 App!</h1>
-      <LoginButton />
-      <Profile />
-      <LogoutButton />
+      {error && <p>Authentication Failed!</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <Profile />
+          <LogoutButton />
+        </>
+      )}
     </div>
   );
 }
